f427fa129cee65365f0af024a44b551f4da30e57
TylenolSniffinTyler <willythewhale8@gmail.com>
2023-01-15 15:55:58 -0500

    Startup

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+        <div id="JesepyName"> Jessepy</div>
+        <div id="Gallery"> Gallery</div>
+        <div id="Murals"> Murals</div>
+        <div id="AboutMe">About Me</div>
+    </div>
+  );
+}
+
+export default NavBar;
---
+++ src/Pyramid.js
@@ -0,0 +1,12 @@
+import './App.css';
+import pryamid from "./assets/Pyramid.png"
+
+const Pyramid = () => {
+    return ( 
+    <div class="Pyramid"> 
+        <img src= { pryamid }/>
+    </div>
+    );
+}
+ 
+export default Pyramid;
